c2c5f7e75da6e2b8a889e39c7635fb597b486f53
HelioHub <helio111171souza@gmail.com>
2024-09-18 08:53:37 -0300

    Project DataSnap Rest VCL FireDAC (OS-Order System)

---
+++ FrontEndOS/FrontEndOS.dpr
@@ -0,0 +1,14 @@
+program FrontEndOS;
+
+uses
+  Vcl.Forms,
+  UOS in 'View\UOS.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
